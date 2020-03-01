3b974cb0d68959f0c5625dd1ea11d6e95ca92df4
dheerajTaiger <dheeraj.r@taiger.com>
2020-03-02 03:08:32 +0800

    Added build ui

---
+++ build/precache-manifest.5731a9eb607a9dfd6762b5bca2908762.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906f856161e9f0704d95dad81992918d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b6ce813046d30670969",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9edaca078ca281aabc5b",
+    "url": "/static/js/2.9d25e9c5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.9d25e9c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b6ce813046d30670969",
+    "url": "/static/js/main.660d2f74.chunk.js"
+  },
+  {
+    "revision": "ccef8cb388aa26d38abc",
+    "url": "/static/js/runtime-main.4290e255.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
