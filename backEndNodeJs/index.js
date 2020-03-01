8a94949518d8de3346cd26d4ce3561b786443357
dheerajTaiger <dheeraj.r@taiger.com>
2020-03-01 12:18:47 +0800

    Finished week3-Ex3.8

--- backEndNodeJs/index.js
+++ backEndNodeJs/index.js
@@ -11,8 +11,22 @@ let notes = [
     number: "97959402"
   }
 ];
+
 app.use(bodyParser.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(function(tokens, req, res) {
+    return [
+      tokens.method(req, res),
+      tokens.url(req, res),
+      tokens.status(req, res),
+      tokens.res(req, res, "content-length"),
+      "-",
+      tokens["response-time"](req, res),
+      "ms",
+      JSON.stringify(req.body)
+    ].join(" ");
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello World!</h1>");
